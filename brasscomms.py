dab40525746c331abeeaa9bc02a7fbadc9bd9e33
Ian Voysey <iev@cs.cmu.edu>
2016-09-26 14:21:14 -0400

    stubs for the three API first deliverables

---
+++ brasscomms.py
@@ -0,0 +1,27 @@
+from flask import Flask , request
+app = Flask(__name__)
+
+@app.route('/logs/status/DASSTATUS', methods=['GET'])
+def status():
+    assert request.path == '/logs/status/DASSTATUS'
+    assert request.method == 'GET'
+    return 'todo: make a ROS call here to determine status'
+
+@app.route('/phase1/power/start_challenge_problem', methods=['POST'])
+def startChallengeProblem():
+    assert request.path == '/phase1/power/start_challenge_problem'
+    assert request.method == 'POST'
+
+    return 'todo: make a ROS call here to start the bot\n you posted' + (str (request.form))
+
+@app.route('/phase1/power/stop_challenge_problem', methods=['POST'])
+def stopChallengeProblem():
+    assert request.path == '/phase1/power/stop_challenge_problem'
+    assert request.method == 'POST'
+    return 'todo: make a ROS call here to stop the bot'
+
+
+
+
+if __name__ == "__main__":
+    app.run()
