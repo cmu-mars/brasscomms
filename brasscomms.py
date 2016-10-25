1dfc9ead07253ce1fff96d071e21fe375e1e9901
Ian Voysey <iev@cs.cmu.edu>
2016-10-25 11:13:01 -0400

    tidying up, removing dead todos, pushing things into github issues

--- brasscomms.py
+++ brasscomms.py
@@ -2,10 +2,34 @@ from flask import Flask , request , abort
 from enum import Enum
 app = Flask(__name__)
 
+## some definitions and helper functions
 status = Enum ('Status', 'Starting Operational Adapting ShuttingDown Completed ')
 
-## this could be a horrible concurrency bug; i don't know yet
+## checks to see if a string represents an integer
+def isint(x):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def isbool(x):
+    if (x == 'true' or x == 'false'):
+        return True
+    return False
+
+
+## returns true iff the first argument is a digit inclusively between the
+## second two args. assumes that the second two are indeed digits, and that
+## the second is less than the third.
+def int_out_of_range(x,upper,lower) :
+    return not(isint(x) and x >= lower and x <= upper)
+
+
+## todo: this could be a horrible concurrency bug; i don't know yet.
 start_percentage = -1
+
+
 
 ## subroutines for the first deliverable
 
@@ -30,21 +54,6 @@ def stopChallengeProblem():
 
     return 'todo: make a ROS call here to stop the bot'
 
-## checks to see if a string represents an integer
-def isint(x):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-
-## returns true iff the first argument is a digit inclusively between the
-## second two args. assumes that the second two are indeed digits, and that
-## the second is less than the third.
-def int_out_of_range(x,upper,lower) :
-    return not(isint(x) and x >= lower and x <= upper)
-
 ## subroutines for the rest of the full API
 
 @app.route('/phase1/power/initial_settings', methods=['POST'])
@@ -52,19 +61,15 @@ def initalSettings():
     assert request.path == '/phase1/power/initial_settings'
     assert request.method == 'POST'
 
-    ## how to check that there's nothing else in the args?
-
-    ## todo: type check all these values. they're probably just strings,
-    ## make sure they meet the spec or barf appropriately
     adaptions = request.args.get('enable_adaptions','')
-    if not (adaptions == 'true' or adaptions == 'false'):
+    if not isbool (adaptions):
         abort(400, 'adaptations must be either "true" or "false"')
 
     startPercent = request.args.get('start_battery','')
     if int_out_of_range (startPercent, 0, 100):
         abort(400, 'start percentage out of range, must be between 0 and 100 inclusive')
 
-    # if we're going to use it, set the global as well. this could be a
+    # todo: if we're going to use it, set the global as well. this could be a
     # horrible concurrency bug.
     global start_percentage
     start_percentage = startPercent
@@ -74,9 +79,6 @@ def initalSettings():
     [obsLoc_x , obsLoc_y] = obsLoc.split(',')
     if not (isint(obsLoc_x) and isint(obsLoc_y)):
         abort(400, 'object location is malformed. must of the form "int,int"')
-
-        # todo: the API doesn't specify that these have to be within the
-        # map bounds. should it?
 
     adaptPercent = request.args.get('minimum_battery','')
     if int_out_of_range(adaptPercent, 0, 100) or adaptPercent >= startPercent:
@@ -101,7 +103,7 @@ def changePower():
     assert request.path == '/phase1/power/change_power'
     assert request.method == 'POST'
 
-    ## this could be a horrible concurrency bug.
+    ## todo: this could be a horrible concurrency bug.
     global start_percentage
 
     ## todo: technically, this is not part of the spec given in the API.
@@ -137,10 +139,6 @@ def removeObstacle():
 
     return 'todo: make a call to remove the obstacle here'
 
-## these are under the recalibration heading rather than power; the APIs
-## seem less-well defined, so while they're still stubbed in, it's not
-## totally clear what they will do.
-
 @app.route('/phase1/recalibration/start_challenge_problem', methods=['POST'])
 def recal_start():
     assert request.path == '/phase1/recalibration/start_challenge_problem'
@@ -153,29 +151,66 @@ def recal_init():
     assert request.path == '/phase1/recalibration/initial_settings'
     assert request.method == 'POST'
 
-    ## todo: check that each of these produces something and that it's in range?
     adaptions = request.args.get('enable_adaptions','')
+    if not isbool (adaptions):
+        abort(400, 'adaptations must be either "true" or "false"')
+
     kinect_dx = request.args.get('kinect_dx','')
+    if int_out_of_range(kinect_dx, -5, 5):
+        abort(400, 'initial kinect dx out of range')
+
     kinect_dy = request.args.get('kinect_dy','')
+    if int_out_of_range(kinect_dy, -5, 5):
+        abort(400, 'initial kinect dy out of range')
+
     kinect_dz = request.args.get('kinect_dz','')
+    if int_out_of_range(kinect_dz, -5, 5):
+        abort(400, 'initial kinect dz out of range')
+
     kinect_dr = request.args.get('kinect_dr','')
+    if int_out_of_range(kinect_dr, -30, 30):
+        abort(400, 'initial kinect dr out of range')
+
     kinect_dp = request.args.get('kinect_dp','')
-    kinect_dq = request.args.get('kinect_dw','')
+    if int_out_of_range(kinect_dp, -30, 30):
+        abort(400, 'initial kinect dp out of range')
+
+    kinect_dw = request.args.get('kinect_dw','')
+    if int_out_of_range(kinect_dw, -30, 30):
+        abort(400, 'initial kinect dw out of range')
 
     return 'todo: make a call here; what is this supposed to do, exactly? no spec given'
 
+## todo: are you allowed to call this if you have an initial perturbation
+## or not?
 @app.route('/phase1/recalibration/change_settings', methods=['POST'])
 def recal_change():
     assert request.path == '/phase1/recalibration/change_settings'
     assert request.method == 'POST'
 
-    ## todo: check that each of these produces something and that it's in range
     kinect_dx = request.args.get('kinect_dx','')
+    if int_out_of_range(kinect_dx, -5, 5):
+        abort(400, 'updated kinect dx out of range')
+
     kinect_dy = request.args.get('kinect_dy','')
+    if int_out_of_range(kinect_dy, -5, 5):
+        abort(400, 'updated kinect dy out of range')
+
     kinect_dz = request.args.get('kinect_dz','')
+    if int_out_of_range(kinect_dz, -5, 5):
+        abort(400, 'updated kinect dz out of range')
+
     kinect_dr = request.args.get('kinect_dr','')
+    if int_out_of_range(kinect_dr, -30, 30):
+        abort(400, 'updated kinect dr out of range')
+
     kinect_dp = request.args.get('kinect_dp','')
-    kinect_dq = request.args.get('kinect_dw','')
+    if int_out_of_range(kinect_dp, -30, 30):
+        abort(400, 'updated kinect dp out of range')
+
+    kinect_dw = request.args.get('kinect_dw','')
+    if int_out_of_range(kinect_dw, -30, 30):
+        abort(400, 'updated kinect dw out of range')
 
     return 'todo: make a call here'
 
@@ -192,8 +227,7 @@ def recal_stop():
     return 'todo: make a ROS call to stop the challenge problem'
 
 
-
-
-
+# if you run this script from the command line directly, this causes it to
+# actually launch the little web server
 if __name__ == "__main__":
     app.run()
