60e997c7c07da94845d89cb1b2f76fa5e1158e5f
Ian Voysey <iev@cs.cmu.edu>
2017-01-31 14:15:32 -0500

    lambda order

--- src/map_util.py
+++ src/map_util.py
@@ -17,11 +17,10 @@ def isWaypoint(waypointID):
         raise ValueError('There is more than one waypoint with the same identifier in the waypoint locations config file')
     return len(waypointList) == 1
 
-
 def loadWaypointListFromFile(waypointID):
     waypoint_locations_path = '/home/vagrant/catkin_ws/src/cp_gazebo/maps/Wean-entire-floor4-waypoint-locations.json'
     with open(waypoint_locations_path) as waypoints_file:
         data = json.load(waypoints_file)
         waypoint_list = data["map"]
-        return filter(waypoint_list, lambda waypoint: waypoint["node-id"] == waypointID)
+        return filter(lambda waypoint: waypoint["node-id"] == waypointID, waypoint_list)
 
