1345980d34dc4267dfffa04e82edf46dcf574673
Ian Voysey <iev@cs.cmu.edu>
2017-02-27 12:25:56 -0500

    fixing error in computation of bounding box, per discussion on #59

--- src/brasscomms.py
+++ src/brasscomms.py
@@ -324,10 +324,10 @@ def action_place_obstacle():
     obs_name = gazebo.place_new_obstacle(params.ARGUMENTS.x, params.ARGUMENTS.y)
     if obs_name is not None:
         return action_result({"obstacleid" : obs_name,
-                              "topleft_x" : params.ARGUMENTS.x - 1.2,
-                              "topleft_y" : params.ARGUMENTS.y - 1.2,
+                              "topleft_x" :  params.ARGUMENTS.x - 1.2,
+                              "topleft_y" :  params.ARGUMENTS.y - 1.2,
                               "botright_x" : params.ARGUMENTS.x + 1.2,
-                              "botright_y" : params.ARGUMENTS.x + 1.2})
+                              "botright_y" : params.ARGUMENTS.y + 1.2})
     else:
         log_das(LogError.RUNTIME_ERROR, 'gazebo cant place new obstacle at given x y')
         return th_error()
